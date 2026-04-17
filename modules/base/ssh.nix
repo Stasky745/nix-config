0b14455f30e7cf5de21140dcac1b757003960906
rocribera <roc@mailerlite.com>
2026-04-17 17:33:04 +0200

    feat: git sign commits

--- modules/base/ssh.nix
+++ modules/base/ssh.nix
@@ -2,53 +2,36 @@
 
 let
   cfg = config.my.base.ssh;
+
+  configText =
+    lib.concatMapStrings (p: "Include ${p}\n") cfg.extraIncludes
+    + (if cfg.extraIncludes != [] then "\n" else "")
+    + "Host *\n"
+    + "  User ${cfg.username}\n"
+    + "  ForwardAgent no\n"
+    + "  Compression yes\n"
+    + "  ServerAliveInterval 60\n"
+    + "  AddKeysToAgent yes\n"
+    + lib.optionalString cfg.use1PasswordAgent
+        "  IdentityAgent \"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"\n"
+    + lib.concatStringsSep "" (lib.mapAttrsToList (k: v: "  ${k} ${v}\n") cfg.globalOptions)
+    + lib.optionalString (cfg.extraConfig != "") "\n${cfg.extraConfig}\n";
 in
 {
   options.my.base.ssh = {
-    enable      = lib.mkEnableOption "SSH configuration";
-    username    = lib.mkOption {
-      type        = lib.types.str;
-      default     = username;
-      description = "SSH username for remote hosts";
-    };
-    extraIncludes = lib.mkOption {
-      type        = lib.types.listOf lib.types.str;
-      default     = [];
-      description = "Paths prepended as Include directives at the top of the SSH config";
-    };
+    enable            = lib.mkEnableOption "SSH configuration";
+    username          = lib.mkOption { type = lib.types.str; default = username; };
+    extraIncludes     = lib.mkOption { type = lib.types.listOf lib.types.str; default = []; };
     use1PasswordAgent = lib.mkEnableOption "1Password SSH agent socket as IdentityAgent";
-    globalOptions = lib.mkOption {
-      type        = lib.types.attrsOf lib.types.str;
-      default     = {};
-      description = "Extra options merged into the Host * block";
-    };
-    extraConfig = lib.mkOption {
-      type        = lib.types.lines;
-      default     = "";
-      description = "Additional SSH config appended after all blocks";
-    };
+    globalOptions     = lib.mkOption { type = lib.types.attrsOf lib.types.str; default = {}; };
+    extraConfig       = lib.mkOption { type = lib.types.lines; default = ""; };
   };
 
   config = lib.mkIf cfg.enable {
     home-manager.users.${username} = { ... }: {
-      programs.ssh = {
-        enable   = true;
-        includes = cfg.extraIncludes;
-
-        matchBlocks = {
-          "*" = {
-            user                = cfg.username;
-            forwardAgent        = false;
-            identitiesOnly      = true;
-            compression         = true;
-            serverAliveInterval = 60;
-            extraOptions        = { AddKeysToAgent = "yes"; }
-              // lib.optionalAttrs cfg.use1PasswordAgent { IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""; }
-              // cfg.globalOptions;
-          };
-        };
-
-        extraConfig = cfg.extraConfig;
+      home.file.".ssh/config" = {
+        text = configText;
+        mode = "0600";
       };
     };
   };
